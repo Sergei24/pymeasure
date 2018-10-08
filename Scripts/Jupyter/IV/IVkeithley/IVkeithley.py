215f02cc9eb6eca06fc9b527f07b1c3d7cd4886b
Sergei <sergeikozlov24@yandex.ru>
2018-10-08 16:23:15 +0400

    Check errors

--- Scripts/Jupyter/IV/IVkeithley/IVkeithley.py
+++ Scripts/Jupyter/IV/IVkeithley/IVkeithley.py
@@ -20,7 +20,7 @@ def CurrentValues(rangeA, stepA):
 
 class IV(Procedure):
 
-    current_range = FloatParameter('Maximum Current', units='mkA', default=0.1)
+    current_range = FloatParameter('Maximum Current', units='nA', default=100)
     current_step = FloatParameter('Current Step', units='nA', default=10)
     delay = FloatParameter('Delay Time', units='ms', default=10)
 
@@ -28,20 +28,20 @@ class IV(Procedure):
 
     def startup(self):
         log.info("Setting up instruments")
-        self.meter = Keithley2182("GPIB::25")
+        self.meter = Keithley2182("GPIB::5")
         self.meter.setup()
 
-        self.source = Keithley6220("GPIB::1")
+        self.source = Keithley6220("GPIB::12")
         self.source.enable_auto_range()
-        self.source.source_current_range = self.current_range*1e-6  # calculates in Amperes
-        self.source.complinance_voltage = 100  # Volts
+        self.source.source_current_range = self.current_range*1e-9  # calculates in Amperes
+        self.source.complinance_voltage = 1  # Volts
         self.source.reset()
         self.source.enable_source()
         sleep(2)
 
     def execute(self):
-        currents = CurrentValues(current_range, current_step * 1e-3)  # dont forget to change step from nA to mkA
-        currents *= 1e-6  # and finally change to A
+        currents = CurrentValues(current_range, current_step)
+        currents *= 1e-9  # and finally change to A
         steps = len(currents)
 
         log.info("Starting to sweep through current")
